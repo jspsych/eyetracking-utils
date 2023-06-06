9574e83c2c8dd57e8679c957df5a681178a6dd05
jadeddelta <101148768+jadeddelta@users.noreply.github.com>
2023-06-06 15:20:13 -0400

    refactor mp_processing to allow for custom functions

--- et_util/mp_processing.py
+++ et_util/mp_processing.py
@@ -8,14 +8,81 @@ import mediapipe as mp
 from et_util.process_functions import getRightEye, getLeftEye
 
 
-def extract_mesh_from_video(path: str, mesh):
+def process_webm_to_json(
+        in_path: str,
+        out_path: str,
+        process,
+        overwrite=True,
+        verbose=True):
+    """
+    Processes a directory of .webms into .jsons with a given process function.
+
+    :param in_path: the directory containing the .webm files
+    :param out_path: the directory where the .json files will be written to
+    :param process: the processing function taking in the path of the .webm and the face mesh,
+    outputting a json object of the data extracted from the .webm
+    :param overwrite: True if the files in the output directory should be overwritten
+    :param verbose: True if print statements showing the processed points should be displayed
+    """
+    all_files = os.listdir(in_path)
+    mp_face_mesh = mp.solutions.face_mesh
+    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, refine_landmarks=True)
+
+    unique_subjects = set([fname.split('_')[0] for fname in all_files])
+    for subject in unique_subjects:
+        out_file = os.path.join(out_path, subject + '.json')
+        if overwrite and os.path.exists(out_file):
+            os.remove(out_file)
+            if verbose: print("Overwriting " + out_path)
+        if not overwrite and os.path.exists(out_file):
+            print(out_path + " already exists")
+            continue
+
+        all_data = {}
+        subject_data = []
+        subject_files = fnmatch.filter(all_files, subject + "*.webm")
+
+        for fname in subject_files:
+            finfo = fname.replace('.', '_').split('_')
+            subject = finfo[0]
+            block = finfo[1]
+            phase = finfo[2]
+            x = finfo[3]
+            y = finfo[4]
+            tag_json = {
+                'block': block,
+                'phase': phase,
+                'x': x,
+                'y': y
+            }
+            data_json = process(in_path + fname, face_mesh)
+            tag_json.update(data_json)
+            subject_data.append(tag_json)
+            if verbose: print("Processed point [" + x + ", " + y + "]")
+
+        all_data[subject] = subject_data
+
+        with open(out_file, 'w') as file:
+            json.dump(all_data, file)
+            if verbose:
+                print("Generated " + out_file)
+
+
+def get_landmarks(path, face_mesh):
+    """
+    A process function that gets just MediaPipe facial landmarks.
+
+    :param path: the path of the .webm video
+    :param face_mesh: the MediaPipe face mesh
+    :return: a .json containing an array with each frame's facial landmarks
+    """
     cap = cv2.VideoCapture(path)
     out = []
 
     while cap.isOpened():
         ret, frame = cap.read()
-        if ret:
-            results = mesh.process(frame)
+        if ret and frame is not None:
+            results = face_mesh.process(frame)
             if not results.multi_face_landmarks:
                 continue
             landmarks = results.multi_face_landmarks[0].landmark
@@ -27,126 +94,76 @@ def extract_mesh_from_video(path: str, mesh):
     size = len(out)
     if size > 0:
         out = np.reshape(np.array(out), (size, -1, 3)).tolist()
-    return out
+    return {"landmarks": out}
 
 
-def process_webm_to_json(
-        in_path: str,
-        out_path: str,
-        overwrite=True,
-        verbose=True):
-    all_files = os.listdir(in_path)
-    mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, refine_landmarks=True)
-
-    unique_subjects = set([fname.split('_')[0] for fname in all_files])
-    for subject in unique_subjects:
-        out_file = os.path.join(out_path, subject + '.json')
-        if overwrite and os.path.exists(out_file):
-            os.remove(out_file)
-            if verbose: print("Overwriting " + out_path)
-        if not overwrite and os.path.exists(out_file):
-            print(out_path + " already exists")
-            continue
-
-        all_data = {}
-        subject_data = []
-        subject_files = fnmatch.filter(all_files, subject + "*.webm")
-
-        for fname in subject_files:
-            finfo = fname.replace('.', '_').split('_')
-            subject = finfo[0]
-            block = finfo[1]
-            phase = finfo[2]
-            x = finfo[3]
-            y = finfo[4]
-            mesh_features = extract_mesh_from_video(in_path + fname, face_mesh)
-            subject_data.append({
-                'block': block,
-                'phase': phase,
-                'x': x,
-                'y': y,
-                'features': mesh_features
-            })
-            if verbose: print("Processed point [" + x + ", " + y + "]")
-
-        all_data[subject] = subject_data
-
-        with open(out_file, 'w') as file:
-            json.dump(all_data, file)
-            if verbose:
-                print("Generated " + out_file)
-
-
-def process_webm_to_json_modified(
-        in_path: str,
-        out_path: str,
-        overwrite=True,
-        verbose=True):
-    """ Modified process_webm_to_json function that includes images of the left
-    and right eyes in the json data
+def get_landmarks_and_eyedata(path, face_mesh):
     """
-    all_files = os.listdir(in_path)
-    mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=True, refine_landmarks=True)
+    A process function that gets MediaPipe facial landmarks and the image data
+    of the left and right eye.
 
-    class NumpyEncoder(json.JSONEncoder):
-        """ Special json encoder for numpy types """
+    :param path: the path of the .webm video
+    :param face_mesh: the MediaPipe face mesh
+    :return: a .json containing an array with each frame's facial landmarks and eye image data
+    """
+    cap = cv2.VideoCapture(path)
+    out = []
+    left = []
+    right = []
 
-        def default(self, obj):
-            if isinstance(obj, np.integer):
-                return int(obj)
-            elif isinstance(obj, np.floating):
-                return float(obj)
-            elif isinstance(obj, np.ndarray):
-                return obj.tolist()
-            return json.JSONEncoder.default(self, obj)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret and frame is not None:
+            results = face_mesh.process(frame)
+            if not results.multi_face_landmarks:
+                continue
+            landmarks = results.multi_face_landmarks[0].landmark
+            lm_arr = [[lm.x, lm.y, lm.z] for lm in landmarks]
+            out.append(lm_arr)
+            left.append(getLeftEye(frame, lm_arr).tolist())
+            right.append(getRightEye(frame, lm_arr).tolist())
+        else:
+            break
+    size = len(out)
+    if size > 0:
+        out = np.reshape(np.array(out), (size, -1, 3)).tolist()
 
-    unique_subjects = set([fname.split('_')[0] for fname in all_files])
-    for subject in unique_subjects:
-        out_file = os.path.join(out_path, subject + '.json')
-        if overwrite and os.path.exists(out_file):
-            os.remove(out_file)
-            if verbose: print("Overwriting " + out_path)
-        if not overwrite and os.path.exists(out_file):
-            print(out_path + " already exists")
-            continue
+    return {
+        "landmarks": out,
+        "left": left,
+        "right": right
+    }
 
-        all_data = {}
-        subject_data = []
-        subject_files = fnmatch.filter(all_files, subject + "*.webm")
 
-        for fname in subject_files:
-            video = in_path + fname
-            finfo = fname.replace('.', '_').split('_')
-            subject = finfo[0]
-            block = finfo[1]
-            phase = finfo[2]
-            x = finfo[3]
-            y = finfo[4]
-            mesh_features = extract_mesh_from_video(video, face_mesh)
-            left = []
-            right = []
-            for i in range(0, len(mesh_features)):
-                cap = cv2.VideoCapture(video)
-                ret, frame = cap.read()
-                left.append(getLeftEye(frame, mesh_features[i]))
-                right.append(getRightEye(frame, mesh_features[i]))
-            subject_data.append({
-                'block': block,
-                'phase': phase,
-                'x': x,
-                'y': y,
-                'features': mesh_features,
-                'left_eyes': left,
-                'right_eyes': right
-            })
-            if verbose: print("Processed point [" + x + ", " + y + "]")
+def get_everything(path, face_mesh):
+    """
+    A process function that gets the MediaPipe facial landmarks and the whole frame image data.
 
-        all_data[subject] = subject_data
-        dumped = json.dumps(all_data, cls=NumpyEncoder)
+    :param path: the path of the .webm video
+    :param face_mesh: the MediaPipe face mesh
+    :return: a .json containing an array with each frame's facial landmarks and frame image data.
+    """
+    cap = cv2.VideoCapture(path)
+    out = []
+    image = []
 
-        with open(out_file, 'w') as file:
-            json.dump(dumped, file)
-            if verbose:
-                print("Generated " + out_file)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret and frame is not None:
+            results = face_mesh.process(frame)
+            if not results.multi_face_landmarks:
+                continue
+            landmarks = results.multi_face_landmarks[0].landmark
+            lm_arr = [[lm.x, lm.y, lm.z] for lm in landmarks]
+            out.append(lm_arr)
+            image.append(frame.tolist())
+        else:
+            break
+    size = len(out)
+    if size > 0:
+        out = np.reshape(np.array(out), (size, -1, 3)).tolist()
+
+    return {
+        "landmarks": out,
+        "image": image
+    }
