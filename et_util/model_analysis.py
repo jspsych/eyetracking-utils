7237f71022936a48d570a74c51f5357277ce0782
cmikul <126927661+cmikul@users.noreply.github.com>
2023-06-14 09:46:02 -0400

    Update model_analysis.py

--- et_util/model_analysis.py
+++ et_util/model_analysis.py
@@ -4,82 +4,81 @@ import numpy as np
 import random
 
 from et_util.custom_loss import normalized_weighted_euc_dist
+    
+def plot_model_performance(test_data_size, num_points, dataset, predictions):
+   """
+   Plots performance of model and displays average distance and variance of predictions.
 
+   :param test_data_size: size of test data
+   :param num_points: number of points to be plotted
+   :param dataset: a non-shuffled, non-sliced dataset
+   :param predictions: output of model based on test data
+   """
+  dataset_size = dataset.cardinality().numpy()
+  test_data = list(dataset.skip(dataset_size - test_data_size))
 
-def plot_model_performance(dataset_size, test_data_size, num_points, dataset, predictions):
-    """
-    Plots performance of model and displays average distance and variance of predictions.
+  plt.xlim(0,100)
+  plt.ylim(100,0)
+  plt.rcParams.update({'font.size': 7})
+  
+  indexes = []
+  for i in range(0, num_points):
+    indexes += [random.randint(0, test_data_size)]
+  
+  distances = []
+  for i in range(0, num_points):
+    label = np.array(test_data[indexes[i]][1])
+    lx = label[0]
+    ly = label[1]
 
-    :param dataset_size: size of dataset
-    :param test_data_size: size of test data
-    :param num_points: number of points to be plotted
-    :param dataset: a non-shuffled, non-sliced dataset
-    :param predictions: output of model based on test data
-    """
-    test_data = list(dataset.skip(dataset_size - test_data_size))
+    px = predictions[:,0][indexes[i]]
+    py = predictions[:,1][indexes[i]]
 
-    plt.xlim(0, 100)
-    plt.ylim(100, 0)
-    plt.rcParams.update({'font.size': 7})
+    plt.scatter(lx, ly, c="blue", zorder=2)
+    plt.scatter(px, py, c="red", zorder=3)
 
-    indexes = []
-    for i in range(0, num_points):
-        indexes += [random.randint(0, test_data_size)]
+    x_values = [lx, px]
+    y_values = [ly, py]
 
-    distances = []
-    for i in range(0, num_points):
-        label = np.array(test_data[indexes[i]][1])
-        lx = label[0]
-        ly = label[1]
+    plt.plot(x_values, y_values, linestyle="--", zorder=1, color=(0.8, 0.8, 0.8))
 
-        px = predictions[:, 0][indexes[i]]
-        py = predictions[:, 1][indexes[i]]
+    distance = np.array(loss.normalized_weighted_euc_dist([lx, ly], [px, py]))
+    distances += [distance]
 
-        plt.scatter(lx, ly, c="blue", zorder=2)
-        plt.scatter(px, py, c="red", zorder=3)
+    plt.text(lx+2.0, ly+1.0, "D: {:0.2f}".format(distance[0]))
+  
+  distances_all = []
+  predicted_points_x = []
+  predicted_points_y = []
+  for i in range(0, test_data_size):
+    label = np.array(test_data[i][1])
 
-        x_values = [lx, px]
-        y_values = [ly, py]
+    px = predictions[:,0][i]
+    py = predictions[:,1][i]
 
-        plt.plot(x_values, y_values, linestyle="--", zorder=1, color=(0.8, 0.8, 0.8))
+    predicted_points_x += [px]
+    predicted_points_y += [py]
 
-        distance = np.array(normalized_weighted_euc_dist([lx, ly], [px, py]))
-        distances += [distance]
+    distance = np.array(loss.normalized_weighted_euc_dist(label, [px, py]))
+    distances_all += [distance]
 
-        plt.text(lx + 2.0, ly + 1.0, "D: {:0.2f}".format(distance[0]))
+  avg_distance = np.mean(distances_all)
+  avg_x = np.mean(predicted_points_x)
+  avg_y = np.mean(predicted_points_y)
 
-    distances_all = []
-    predicted_points_x = []
-    predicted_points_y = []
-    for i in range(0, test_data_size):
-        label = np.array(test_data[i][1])
+  square_diff_x = []
+  square_diff_y = []
+  for i in range(0, test_data_size):
+    square_diff_x += [np.square(predicted_points_x[i] - avg_x)]
+    square_diff_y += [np.square(predicted_points_y[i] - avg_y)]
+  
+  variance_x = sum(square_diff_x) / test_data_size
+  variance_y = sum(square_diff_y) / test_data_size
 
-        px = predictions[:, 0][i]
-        py = predictions[:, 1][i]
-
-        predicted_points_x += [px]
-        predicted_points_y += [py]
-
-        distance = np.array(normalized_weighted_euc_dist(label, [px, py]))
-        distances_all += [distance]
-
-    avg_distance = np.mean(distances_all)
-    avg_x = np.mean(predicted_points_x)
-    avg_y = np.mean(predicted_points_y)
-
-    square_diff_x = []
-    square_diff_y = []
-    for i in range(0, test_data_size):
-        square_diff_x += [np.square(predicted_points_x[i] - avg_x)]
-        square_diff_y += [np.square(predicted_points_y[i] - avg_y)]
-
-    variance_x = sum(square_diff_x) / test_data_size
-    variance_y = sum(square_diff_y) / test_data_size
-
-    plt.text(80, 16, "Avg. Dist: {:0.2f}".format(avg_distance), color="green", weight="bold")
-    plt.text(75, 21, "Var. of Pred x: {:0.2f}".format(variance_x), color="green", weight="bold")
-    plt.text(75, 26, "Var. of Pred y: {:0.2f}".format(variance_y), color="green", weight="bold")
-    blue_patch = mpatches.Patch(color="blue", label="Labels")
-    red_patch = mpatches.Patch(color='red', label='Predictions')
-    plt.legend(loc="upper right", handles=[red_patch, blue_patch])
-    plt.show()
+  plt.text(80, 16, "Avg. Dist: {:0.2f}".format(avg_distance), color = "green", weight="bold")
+  plt.text(75, 21, "Var. of Pred x: {:0.2f}".format(variance_x), color = "green", weight="bold")
+  plt.text(75, 26, "Var. of Pred y: {:0.2f}".format(variance_y), color = "green", weight="bold")
+  blue_patch = mpatches.Patch(color="blue", label="Labels")
+  red_patch = mpatches.Patch(color='red', label='Predictions')
+  plt.legend(loc="upper right", handles=[red_patch, blue_patch])
+  plt.show()
