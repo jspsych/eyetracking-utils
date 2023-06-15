36697501ff1f9879ba1e88a6b19f610f59e75f55
j-e-man <82974781+j-e-man@users.noreply.github.com>
2023-06-15 15:07:02 -0400

    Add parsing func for jpgs in dataset_utils.py

--- et_util/dataset_utils.py
+++ et_util/dataset_utils.py
@@ -201,3 +201,33 @@ def parse_tfr_element_mediapipe(element):
     feature = tf.io.parse_tensor(landmarks, out_type=tf.float32)
     feature = tf.reshape(feature, shape=(478, 3))
     return (feature, label)
+  
+ def parse_tfr_element_jpg(element):
+    """Process function that parses a tfr element in a raw dataset for get_dataset function.
+      Gets raw image, image height, image width, subject id, and xy labels. 
+
+    """
+
+    data_structure = {
+    'height': tf.io.FixedLenFeature([], tf.int64),
+    'width':tf.io.FixedLenFeature([], tf.int64),
+    'x':tf.io.FixedLenFeature([], tf.float32),
+    'y':tf.io.FixedLenFeature([], tf.float32),
+    'raw_image' : tf.io.FixedLenFeature([], tf.string),
+    'subject_id':tf.io.FixedLenFeature([], tf.int64),
+  }
+
+    content = tf.io.parse_single_example(element, data_structure)
+
+    raw_image = content['raw_image']
+    height = content['height']
+    width = content['width']
+    depth = 3
+    label = [content['x'], content['y']]
+    subject_id = content['subject_id']
+
+    #get our 'feature'-- our image -- and reshape it appropriately
+    image = tf.io.decode_jpeg(raw_image)
+    image = tf.reshape(image, shape=(640, 480, 3))
+
+    return (image, label, subject_id)
