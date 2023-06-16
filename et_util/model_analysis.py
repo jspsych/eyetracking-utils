a41ac84a8281631b148d5e39ca802ab99d08f6e1
cmikul <126927661+cmikul@users.noreply.github.com>
2023-06-16 12:41:49 -0400

    Update model_analysis.py
    Changed plot_model_performance so that it accepts a custom function as a parameter that gets an array of test coordinates for data of a certain shape.

--- et_util/model_analysis.py
+++ et_util/model_analysis.py
@@ -3,20 +3,43 @@ import matplotlib.patches as mpatches
 import numpy as np
 import random
 
-from et_util.custom_loss import normalized_weighted_euc_dist
+def normalized_weighted_euc_dist(y_true, y_pred):
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+
+    # Weighting treats y-axis as unit-scale and creates a rectangle that's 177.8x100 units.  
+    x_weight = tf.constant([1.778, 1.0], dtype=tf.float32)
+
+    # Multiply x-coordinate by 16/9 = 1.778
+    y_true_weighted = tf.math.multiply(x_weight, y_true)
+    y_pred_weighted = tf.math.multiply(x_weight, y_pred)
+
+    # Calculate Euclidean distance with weighted coordinates
+    loss = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(y_pred_weighted - y_true_weighted), axis=-1))
+
+    # Euclidean Distance from [0,0] to [177.8, 100] = 203.992
+    norm_scale = tf.constant([203.992], dtype=tf.float32)
+
+    # Normalizes loss values to the diagonal-- makes loss easier to interpret
+    normalized_loss = tf.math.divide(loss, norm_scale) * 100
+
+    return normalized_loss
     
-def plot_model_performance(dataset_size, test_data_size, num_points, dataset, predictions):
+def plot_model_performance(num_points, test_data, predictions, function):
    """
    Plots performance of model and displays average distance and variance of predictions.
     
-   :param dataset_size: size of dataset
-   :param test_data_size: size of test data
    :param num_points: number of points to be plotted
-   :param dataset: a non-shuffled, non-sliced dataset
+   :param test_data: test data used to make predictions
    :param predictions: output of model based on test data
+   :param function: function that outputs array of labels for data of certain shape
    """
-   test_data = list(dataset.skip(dataset_size - test_data_size))
-
+   
+   test_data_size = 0
+   for element in test_data:
+     test_data_size += 1
+   test_labels_arr = function(test_data)
+   
    plt.xlim(0,100)
    plt.ylim(100,0)
    plt.rcParams.update({'font.size': 7})
@@ -27,7 +50,7 @@ def plot_model_performance(dataset_size, test_data_size, num_points, dataset, pr
   
    distances = []
    for i in range(0, num_points):
-     label = np.array(test_data[indexes[i]][1])
+     label = np.array(test_labels_arr[indexes[i]])
      lx = label[0]
      ly = label[1]
 
@@ -51,7 +74,7 @@ def plot_model_performance(dataset_size, test_data_size, num_points, dataset, pr
    predicted_points_x = []
    predicted_points_y = []
    for i in range(0, test_data_size):
-     label = np.array(test_data[i][1])
+     label = np.array(test_labels_arr[i])
 
      px = predictions[:,0][i]
      py = predictions[:,1][i]
@@ -82,3 +105,12 @@ def plot_model_performance(dataset_size, test_data_size, num_points, dataset, pr
    red_patch = mpatches.Patch(color='red', label='Predictions')
    plt.legend(loc="upper right", handles=[red_patch, blue_patch])
    plt.show()
+
+
+def gen_test_arr_landmarks(test_data):
+  """Helper function for plot_model_performance that generates
+  array of test points from test data in format landmarks, label
+  :param test_data:
+  :return: array of test points"""
+  arr = [label for landmarks, label in test_data]
+  return arr
